f8f71e334134163b67ed49e823762afffdc8f60f
Ajahar Husain <Ajahar-Husain@users.noreply.github.com>
2026-02-06 22:21:14 +0530

    Connect to Vercel and Backend Connected to AWS

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,7 @@
 let IS_PROD = true;
-const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
-
-    "http://localhost:8000"
+const server = process.env.NODE_ENV === 'production' 
+    ? (process.env.REACT_APP_API_URL || "https://apnacollegebackend.onrender.com")
+    : "http://localhost:8000";
 
 
 export default server;
